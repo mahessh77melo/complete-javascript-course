cf09b2112676577531c8770464570be67968fe08
mahessh77melo <magesh.lbj23@gmail.com>
2020-10-22 08:55:05 +0530

    my first commit 🔥

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printer = statement => console.log(statement);
+printer('hello world in version 2');
+console.log('🔥');
