8733454ee0f2a258ede0982d35a5032825ed98ed
mahessh77melo <magesh.lbj23@gmail.com>
2021-02-24 18:02:54 +0530

    first commit

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -10,4 +10,33 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 
+// api key => 1b72ce05-f020-4c6a-a734-4303be611a13
+
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+    const obj = data.data.recipe;
+    // throw an error if the resulting object is not what we expected
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    //  altering the property names
+    const recipe = {
+      id: obj.id,
+      image: obj.image_url,
+      cookingTime: obj.cooking_time,
+      url: obj.source_url,
+      publisher: obj.publisher,
+      ingredients: obj.ingredients,
+      servings: obj.servings,
+      title: obj.title,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    alert(error);
+  }
+};
+showRecipe();
